3146e4f99f72e0f683b73a32f42ccf16a12b15d5
rajp01 <95058343+rajp01@users.noreply.github.com>
2021-12-31 22:32:21 +0530

    Add files via upload

---
+++ StockerMain.java
@@ -0,0 +1,31 @@
+package com.greatlearning.service;
+import java.util.*;
+import java.io.*;
+
+public class StockerMain {
+	
+	    public static void main(String args[])
+	    {
+	        
+	        Stack<Integer> stack = new Stack<Integer>();
+	        stack.push(10);
+	        stack.push(15);
+	        stack.push(30);
+	        stack.push(20);
+	        stack.push(5);
+	 
+	        
+	        System.out.println("Initial Stack: " + stack);
+	 
+	       
+	        System.out.println("Popped element: "
+	                           + stack.pop());
+	        System.out.println("Popped element: "
+	                           + stack.pop());
+	 
+	        
+	        System.out.println("Stack after pop operation "
+	                           + stack);
+	    }
+
+}
